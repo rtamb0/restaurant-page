771757cd0b6b3156b0ce73fef669db54bf5f82f2
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-06-28 10:12:55 +0700

    Create module for home page

---
+++ src/home.js
@@ -0,0 +1,20 @@
+const contentDOM = document.querySelector('#content');
+
+const textDOMs = {
+    header: document.createElement('h1'),
+    description: document.createElement('p'),
+    section: document.createElement('section'),
+}
+
+const text = (() => {
+    textDOMs.header.innerHTML = 'Lorem Ipsum';
+    textDOMs.section.appendChild(textDOMs.header)
+    textDOMs.description.innerHTML = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
+    textDOMs.section.appendChild(textDOMs.description);
+})();
+
+const appendHome = () => {
+    contentDOM.appendChild(textDOMs.section);    
+}
+
+export {appendHome};
