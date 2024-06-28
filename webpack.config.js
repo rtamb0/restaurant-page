8b44b1c276c35b6e41c63ce0af2dc232a60185bd
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-06-28 11:07:01 +0700

    Add source map for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Restaurant Page',
-      template: './src/index.html'
-    }),
-  ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Page',
+            template: './src/index.html'
+        }),
     ],
-  },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
