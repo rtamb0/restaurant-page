270dffe4ef339702224e921511775d2e0f9ac1a8
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-01 13:24:01 +0700

    Wrap body contents in a container

--- src/home.js
+++ src/home.js
@@ -1,5 +1,8 @@
 const contentDOM = document.querySelector('#content');
 
+const bodyContainer = document.createElement('section');
+bodyContainer.className = 'bodyHome';
+
 // Function to generate header
 const headContent = (title, text) => {
     const header = document.createElement('h1');
@@ -19,16 +22,19 @@ const bodyContent = (title, text) => {
     header.innerHTML = title;
     const description = document.createElement('p');
     description.innerHTML = text;
-    const section = document.createElement('section');
+    const section = document.createElement('div');
     section.appendChild(header);
     section.appendChild(description);
-    section.className = 'bodyHome';
     return section;
 }
 
 // Displays home page
 const appendHome = () => {
     contentDOM.appendChild(headContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+    bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+    bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+    bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+    contentDOM.appendChild(bodyContainer);
     contentDOM.className = "home";    
 }
 
