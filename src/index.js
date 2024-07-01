5443e98e942be73412fdd39c3659414cabaa7449
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-01 16:20:05 +0700

    Add button selectors

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import 'normalize.css';
 import './style.css';
 import { appendHome } from "./home";
 
+const buttonDOMs = {
+    home: document.querySelector('#home'),
+    menu: document.querySelector('#menu'),
+    contact: document.querySelector('#contact'),
+    about: document.querySelector('#about'),
+}
+
+
 appendHome()
