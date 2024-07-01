ba78bfa8e85fccd72aec70bdb4a8e355c5e095d3
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-01 10:23:08 +0700

    Change IIFEs and objects into functions that get called when it gets appended

--- src/home.js
+++ src/home.js
@@ -1,28 +1,34 @@
 const contentDOM = document.querySelector('#content');
 
-const headDOMs = {
-    header: document.createElement('h1'),
-    description: document.createElement('p'),
-    section: document.createElement('section'),
+// Function to generate header
+const headContent = (title, text) => {
+    header = document.createElement('h1');
+    header.innerHTML = title;
+    description = document.createElement('p');
+    description.innerHTML = text;
+    section = document.createElement('section');
+    section.appendChild(header);
+    section.appendChild(description);
+    section.className = 'headHome';
+    return section;
 }
 
-const bodyDOMs = {
-    section: document.createElement('section'),
+// Function to generate body content
+const bodyContent = (title, text) => {
+    header = document.createElement('h3');
+    header.innerHTML = title;
+    description = document.createElement('p');
+    description.innerHTML = text;
+    section = document.createElement('section');
+    section.appendChild(header);
+    section.appendChild(description);
+    section.className = 'bodyHome';
+    return section;
 }
 
-const headText = (() => {
-    headDOMs.header.innerHTML = 'Lorem Ipsum';
-    headDOMs.section.appendChild(headDOMs.header)
-    headDOMs.description.innerHTML = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
-    headDOMs.section.appendChild(headDOMs.description);
-})();
-
-const bodyText = (() => {
-    
-})();
-
+// Displays home page
 const appendHome = () => {
-    contentDOM.appendChild(headDOMs.section);
+    contentDOM.appendChild(headContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
     contentDOM.className = "home";    
 }
 
