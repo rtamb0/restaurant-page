58b006bc2b6bdbae9d2c3ef0cac5d6a171b1dcac
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-01 10:01:59 +0700

    Categorise head elements more accordingly and add body elements categorisation

--- src/home.js
+++ src/home.js
@@ -1,21 +1,28 @@
 const contentDOM = document.querySelector('#content');
 
-const textDOMs = {
+const headDOMs = {
     header: document.createElement('h1'),
     description: document.createElement('p'),
     headSection: document.createElement('section'),
+}
+
+const bodyDOMs = {
     bodySection: document.createElement('section'),
 }
 
 const headText = (() => {
-    textDOMs.header.innerHTML = 'Lorem Ipsum';
-    textDOMs.headSection.appendChild(textDOMs.header)
-    textDOMs.description.innerHTML = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
-    textDOMs.headSection.appendChild(textDOMs.description);
+    headDOMs.header.innerHTML = 'Lorem Ipsum';
+    headDOMs.headSection.appendChild(headDOMs.header)
+    headDOMs.description.innerHTML = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...";
+    headDOMs.headSection.appendChild(headDOMs.description);
+})();
+
+const bodyText = (() => {
+    
 })();
 
 const appendHome = () => {
-    contentDOM.appendChild(textDOMs.headSection);
+    contentDOM.appendChild(headDOMs.headSection);
     contentDOM.className = "home";    
 }
 
