f63832747df76e81f01f08502f80179a204a68a2
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-02 09:53:04 +0700

    Declare bodyContainer in appendHome to prevent cloning

--- src/home.js
+++ src/home.js
@@ -1,17 +1,17 @@
 const contentDOM = document.querySelector('#content');
 
-const bodyContainer = document.createElement('section');
-bodyContainer.className = 'bodyHome';
-
 // Function to generate header
 const headContent = (title, text) => {
     const header = document.createElement('h1');
     header.innerHTML = title;
+    
     const description = document.createElement('p');
     description.innerHTML = text;
+    
     const section = document.createElement('section');
     section.appendChild(header);
     section.appendChild(description);
+    
     section.className = 'headHome';
     return section;
 }
@@ -20,22 +20,29 @@ const headContent = (title, text) => {
 const bodyContent = (title, text) => {
     const header = document.createElement('h3');
     header.innerHTML = title;
+    
     const description = document.createElement('p');
     description.innerHTML = text;
+    
     const section = document.createElement('div');
     section.appendChild(header);
     section.appendChild(description);
+    
     return section;
 }
 
 // Displays home page
 const appendHome = () => {
     contentDOM.appendChild(headContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+    
+    const bodyContainer = document.createElement('section');
+    bodyContainer.className = 'bodyHome';
     bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
     bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
     bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
     contentDOM.appendChild(bodyContainer);
-    contentDOM.className = "home";    
+    
+    contentDOM.className = "home";
 }
 
 export {appendHome};
