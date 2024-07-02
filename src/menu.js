c0864e5d4c53acf622d34e80eb19af73db758343
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-02 09:54:33 +0700

    Declare bodyContainer in appendHome to prevent cloning

--- src/menu.js
+++ src/menu.js
@@ -2,26 +2,31 @@ import recipe1 from './assets/recipe1.jpg';
 
 const contentDOM = document.querySelector('#content');
 
-const bodyContainer = document.createElement('section');
-bodyContainer.className = 'bodyMenu';
-
 const bodyContent = (title, text, pic) => {
     const img = document.createElement('img');
     img.setAttribute('src', pic);
+    
     const header = document.createElement('h3');
     header.innerHTML = title;
+    
     const description = document.createElement('p');
     description.innerHTML = text;
+    
     const section = document.createElement('div');
     section.appendChild(img);
     section.appendChild(header);
     section.appendChild(description);
+    
     return section;
 }
 
 const appendMenu = () => {
-    contentDOM.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...", recipe1));
+    const bodyContainer = document.createElement('section');
+    bodyContainer.className = 'bodyMenu';
+    
+    bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...", recipe1));
     contentDOM.appendChild(bodyContainer);
+    
     contentDOM.className = "menu";    
 }
 
