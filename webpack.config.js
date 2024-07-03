84f7b8098249066f3143f2402caa2f8c9dbd6dc0
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-03 17:21:43 +0700

    Add formatting for font asset

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,9 @@ module.exports = {
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
           type: 'asset/resource',
+          generator: {
+            filename: '[name]-[hash][ext]'
+          }
         },
       ],
     },
