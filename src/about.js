2ca78b3ebfa66d716c5a8c0304ddfa5dd6bdb5f2
rtamb0 <135024553+rtamb0@users.noreply.github.com>
2024-07-05 14:20:13 +0700

    Change texts in about page

--- src/about.js
+++ src/about.js
@@ -42,11 +42,11 @@ const appendAbout = () => {
     const bodyContainer = document.createElement('section');
     bodyContainer.className = 'bodyAbout';
     
-    bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+    bodyContainer.appendChild(bodyContent("Our Story", "Founded with a deep respect for the ranching heritage, Cattleman’s Corner is more than just a steakhouse – it's a tribute to the hardworking spirit of the West. Our chefs source the finest cuts of beef and freshest ingredients, crafting each dish with care and precision. From our rustic décor to our welcoming hospitality, we strive to create an authentic Western ambiance that makes every visit memorable."));
     bodyContainer.appendChild(picContent(about1, "A pic of a guy serving hamburger"));
     bodyContainer.appendChild(picContent(about2, "A pic of a steak dish"));
-    bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
-    bodyContainer.appendChild(bodyContent("Lorem Ipsum", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+    bodyContainer.appendChild(bodyContent("Our Chefs", "Our talented chefs are the heart of Cattleman’s Corner. With years of experience and a passion for Western cuisine, they create mouthwatering dishes that honor the traditions of the American frontier. From expertly grilled steaks to innovative side dishes, our culinary team is dedicated to delivering a dining experience that is second to none."));
+    bodyContainer.appendChild(bodyContent("Our Location", "Nestled in the heart of yours truly, Cattleman’s Corner combines the rustic charm of a Western ranch with the modern comforts of a premier dining establishment. Conveniently located and easy to find, our restaurant is the perfect place for both locals and visitors to enjoy a hearty meal and warm hospitality."));
     contentDOM.appendChild(bodyContainer);
 
     contentDOM.className = "about";
